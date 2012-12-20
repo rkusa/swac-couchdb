d71a71da1ff8a4f07403e2fa5ecdd1015a2dcfc7
Markus Ast <m@rkusa.st>
2012-12-20 17:53:23 +0100

    Refactor API Methods into Definition Class
    * refactor API methods into definition class
    * update API method's arguments to newest adapter spec
    * remove extra get calls on put and delete
    * group redudant code into methods

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var nano = require('nano')(process.env.CLOUDANT_URL || 'http://localhost:5984')
 
-var Definition = function(db, design, define, callback) {
+var API = function(db, model, define, callback) {
   this.db = db
-  this.design = design
+  this.model = model
+  this.design = '_design/' + model._type
   this.queue = []
   this.views = {}
   this.callback = function() {
@@ -19,7 +20,7 @@ var Definition = function(db, design, define, callback) {
   })
 }
 
-Definition.prototype.view = function(name, view) {
+API.prototype.view = function(name, view) {
   var that = this
 
   if (typeof view === 'function') {
@@ -44,119 +45,124 @@ Definition.prototype.view = function(name, view) {
   }
 }
 
-exports.initialize = function(model, opts, define, cb) {
+API.prototype.extractId = function(id) {
+  return id.indexOf(this.model._type) === 0
+    ? id.substr(this.model._type.length + 1)
+    : id
+}
+
+API.prototype.adaptId = function(id) {
+  if (!id.match(/^[a-z0-9]{32}$/)) id = this.model._type + '/' + id
+  return id
+}
+
+API.prototype.createModel = function(id, data, rev) {
+  data.id        = this.extractId(id)
+  var instance   = new this.model(data)
+  instance._id   = id
+  instance._rev  = rev
+  instance.isNew = false
+  return instance
+}
+
+API.prototype.get = function(id, callback) {
+  if (!callback) callback = function() {}
+  if (!id) return callback(null, null)
+
+  var that = this
+  this.db.get(this.adaptId(id), function(err, body) {
+    if (err) {
+      switch (err.message) {
+        case 'missing':
+        case 'deleted':
+          return callback(null, null)
+        default:
+          return callback(err, null)
+      }
+    }
+
+    callback(null, that.createModel(body._id, body, body._rev))
+  })
+}
+
+API.prototype.list = function(/*view, key, callback*/) {
+  var args = Array.prototype.slice.call(arguments)
+    , that = this
+    , callback = args.pop()
+    , view = args.shift() || 'all'
+    , key = args.shift() || null
+    , params = { include_docs: true }
+
+  if (key)       params.key = key
+  if (!callback) callback = function() {}
+
+  var fn
+  if (this.views[view])
+    fn = this.views[view].bind(this.db, key, process.domain.req)
+  else
+    fn = this.db.view.bind(this.db, this.model._type, view, params)
+
+  fn(function(err, body) {
+    if (err) return callback(err, null)
+    var rows = []
+    body.rows.forEach(function(data) {
+      rows.push(that.createModel(data.id, data.doc, data.doc._rev))
+    })
+    callback(null, rows)
+  })
+}
+
+API.prototype.put = function(instance, callback) {
+  if (!callback) callback = function() {}
+  var data   = instance.toJSON(true)
+  data._rev  = instance._rev
+  data._id   = instance._id
+  data.$type = instance._type
+  delete data.id
+  this.db.insert(data, instance._id, function(err, res) {
+    if (err) return callback(err, null)
+    instance._rev  = res.rev
+    instance.isNew = false
+    callback(null, instance)
+  })
+}
+
+API.prototype.post = function(props, callback) {
+  if (!callback) callback = function() {}
+
+  if (props instanceof this.model) props = props.toJSON(true)
+  if (props.id) props._id = this.model._type + '/' + props.id
+  delete props.id
+  props.$type = this.model._type
+
+  var that = this
+  this.db.insert(props, props._id, function(err, body) {
+    if (err) return callback(err, null)
+    callback(null, that.createModel(body.id, props, body.rev))
+  })
+}
+
+
+API.prototype.delete = function(instance, callback) {
+  if (!callback) callback = function() {}
+  this.db.destroy(instance._id, instance._rev, function(err) {
+    if (err) return callback(err)
+    callback(null)
+  })
+}
+
+exports.initialize = function(model, opts, define, callback) {
   var db = nano.use(opts.db)
-    , api = {}
-    , definition = new Definition(db, '_design/' + model._type, define, cb)
-  definition.view('all', {
+    , api = new API(db, model, define, callback)
+
+  api.view('all', {
     map: "function (doc) { if(doc.$type === '" + model._type + "') emit(doc._id, null); }"
   })
 
-  api.list = function(/*view, key, callback*/) {
-    var args = Array.prototype.slice.call(arguments)
-      , callback = args.pop()
-      , view = args.shift() || 'all'
-      , key = args.shift() || null
-      , params = { include_docs: true }
-    if (key) params.key = key
-    if (!callback) callback = function() {}
-
-    var fn
-    if (definition.views[view])
-      fn = definition.views[view].bind(db, key, process.domain.req)
-    else
-      fn = db.view.bind(db, model._type, view, params)
-
-    fn(function(err, body) {
-      if (err) return callback(err)
-      var rows = []
-      body.rows.forEach(function(data) {
-        data.doc._id = data.id
-        data.doc.id = data.id.indexOf(model._type) === 0
-                        ? data.id.substr(model._type.length + 1)
-                        : data.id
-        var row = new model(data.doc)
-        row.isNew = false
-        rows.push(row)
-      })
-      callback(null, rows)
-    })
-  }
-  api.get = function(id, callback) {
-    if (!callback) callback = function() {}
-    if (!id) return callback(null, null)
-    if (!id.match(/^[a-z0-9]{32}$/)) id = model._type + '/' + id
-    db.get(id, function(err, body) {
-      if (err) {
-        switch (err.message) {
-          case 'missing':
-          case 'deleted':
-            return callback(null, null)
-          default:
-            return callback(err)
-        }
-      }
-      body.id = body._id.indexOf(model._type) === 0
-                  ? body._id.substr(model._type.length + 1)
-                  : body._id
-      var row = new model(body)
-      row.isNew = false
-      callback(null, row)
-    })
-  }
-  api.put = function(id, props, callback) {
-    if (!callback) callback = function() {}
-    if (!id.match(/^[a-z0-9]{32}$/)) id = model._type + '/' + id
-    db.get(id, function(err, body) {
-      if (err) return callback(err)
-      body.id = body._id.indexOf(model._type) === 0
-                  ? body._id.substr(model._type.length + 1)
-                  : body._id
-      var row = new model(body)
-      row.isNew = false
-      row.updateAttributes(props)
-      var data = row.toJSON()
-      data._id = body._id
-      delete data.id
-      data._rev = body._rev
-      data.$type = model._type
-      db.insert(data, data.id, function(err) {
-        if (err) return callback(err)
-        callback(null, row)
-      })
-    })
-  }
-  api.post = function(props, callback) {
-    if (!callback) callback = function() {}
-    if (props instanceof model) {
-      props = props.toJSON()
-    }
-    if (props.id) props._id = model._type + '/' + props.id
-    delete props.id
-    props.$type = model._type
-    db.insert(props, props._id, function(err, body) {
-      if (err) return callback(err)
-      props._id = body.id
-      props.id = body.id.indexOf(model._type) === 0
-                  ? body.id.substr(model._type.length + 1)
-                  : body.id
-      var row = new model(props)
-      row.isNew = false
-      callback(null, row)
-    })
-  }
-  api.delete = function(id, callback) {
-    if (!callback) callback = function() {}
-    if (!id.match(/^[a-z0-9]{32}$/)) id = model._type + '/' + id
-    db.get(id, function(err, body) {
-      if (err) return callback(err)
-
-      db.destroy(id, body._rev, function(err) {
-        if (err) return callback(err)
-        callback()
-      })
-    })
-  }
+  Object.defineProperties(model.prototype, {
+    _id:  { value: null, writable: true },
+    _rev: { value: null, writable: true } 
+  })
 
   return api
 }
